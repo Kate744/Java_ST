c3b3f61af29fc245dd65546451865b6b85052a7e
Kate744 <kate744@ya.ru>
2019-02-03 18:35:11 -0400

    Distance version 3.0
    Doesn't work

--- Sand/src/main/java/sfwtesting/autojava/Point.java
+++ Sand/src/main/java/sfwtesting/autojava/Point.java
@@ -1,18 +1,27 @@
 package sfwtesting.autojava;
 
 public class Point {
-    public double a1;
-    public double a2;
-    public double b1;
-    public double b2;
 
-    public Point (double a1, double a2, double b1, double b2) {
+    public int a1;
+    public int a2;
+    public int b1;
+    public int b2;
+
+    public Point(int a1, int a2, int b1, int b2) {
         this.a1 = a1;
         this.a2 = a2;
         this.b1 = b1;
         this.b2 = b2;
     }
-    public double distance() {
-        return (Math.sqrt((this.b2 - this.b1) * (this.b2 - this.b1) + (this.a2 - this.a1) * (this.a2 - this.a1)));
+
+    public Point1(int a1, int a2) {
+
+    }
+    public Point2(int b1, int b2) {
+
+    }
+
+    public double distance(Point1, Point2) {
+        return Math.sqrt((this.b2 - this.b1) * (this.b2 - this.b1) + (this.a2 - this.a1) * (this.a2 - this.a1));
     }
 }
--- Sand/src/main/java/sfwtesting/autojava/WT.java
+++ Sand/src/main/java/sfwtesting/autojava/WT.java
@@ -8,15 +8,19 @@ public static void main (String[] args) {
     Square s = new Square(5);
     System.out.println("Square if side a = " + s.l + " equal " + s.areal());
 
-    Rectangle r = new Rectangle(5,7);
+    Rectangle r = new Rectangle(5, 7);
     System.out.println("Square of rectangle if side a" + " = " + r.a + " and side b = " + r.b + " equal " + r.area());
 
-    Point p = new Point(18, 27, 14, 10);
-    System.out.println("Distance between two points is " + p.distance());
+// Домашнее задание 2:
+    Point p1 = new Point(3, 5);
+    Point p2 = new Point(8, 1);
+    System.out.println("Distance between two points is " + distance(p1, p2));
 
 }
-public static void hello(String smb) {
-    System.out.println("Hi " + smb + "!");
-}
+// Конец д.з 2
+
+    public static void hello(String smb) {
+        System.out.println("Hi " + smb + "!");
+    }
 
 };
