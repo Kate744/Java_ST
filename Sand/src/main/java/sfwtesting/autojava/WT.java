10f5f40f20fdd04b41db7c1f62fdacbfbfd28bc1
Kate744 <kate744@ya.ru>
2019-02-02 21:12:52 -0400

    Basic functions

--- Sand/src/main/java/sfwtesting/autojava/WT.java
+++ Sand/src/main/java/sfwtesting/autojava/WT.java
@@ -3,7 +3,25 @@ package sfwtesting.autojava;
 public class WT {
 
 public static void main (String[] args) {
-System.out.println ("Hi man");
+    hello("man");
+
+
+    double al = 5;
+    System.out.println("Square if side a = " + al + " equal " + area(al));
+
+    double a = 5;
+    double b = 7;
+    System.out.println("Square of rectangle if side a" + " = " + a + " and side b = " + b + " equal " + area(a,b));
+}
+public static void hello(String smb) {
+    System.out.println("Hi " + smb + "!");
 }
 
+public static double area(double l) {
+    return l * l;
+}
+
+public static double area (double a, double b) {
+    return a * b;
+}
 };
