74a1fa03c29514e73702fab2c54927fd910a685e
Kate744 <kate744@ya.ru>
2019-02-05 21:18:45 -0400

    Distance. Version 3.0

--- Sand/src/main/java/sfwtesting/autojava/Point.java
+++ Sand/src/main/java/sfwtesting/autojava/Point.java
@@ -2,26 +2,23 @@ package sfwtesting.autojava;
 
 public class Point {
 
-    public int a1;
-    public int a2;
-    public int b1;
-    public int b2;
+    public int x;
+    public int y;
 
-    public Point(int a1, int a2, int b1, int b2) {
-        this.a1 = a1;
-        this.a2 = a2;
-        this.b1 = b1;
-        this.b2 = b2;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+// До
+   /* public double distance(Point p1, Point p2) {
+        return Math.sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
+    };*/
+// После:
+    public double distance(Point p2) {
+        return Math.sqrt((this.x - p2.x) * (this.x - p2.x) + (this.y - p2.y) * (this.y - p2.y));
     }
 
-    public Point1(int a1, int a2) {
+};
 
-    }
-    public Point2(int b1, int b2) {
 
-    }
-
-    public double distance(Point1, Point2) {
-        return Math.sqrt((this.b2 - this.b1) * (this.b2 - this.b1) + (this.a2 - this.a1) * (this.a2 - this.a1));
-    }
-}
--- Sand/src/main/java/sfwtesting/autojava/WT.java
+++ Sand/src/main/java/sfwtesting/autojava/WT.java
@@ -11,13 +11,14 @@ public static void main (String[] args) {
     Rectangle r = new Rectangle(5, 7);
     System.out.println("Square of rectangle if side a" + " = " + r.a + " and side b = " + r.b + " equal " + r.area());
 
-// Домашнее задание 2:
+    // Домашнее задание 2
     Point p1 = new Point(3, 5);
     Point p2 = new Point(8, 1);
-    System.out.println("Distance between two points is " + distance(p1, p2));
 
+   //Было: System.out.println("Distance between two points is " + p1.distance(p1, p2));
+    // Стало:
+    System.out.println("Distance between two points is " + p1.distance(p2));
 }
-// Конец д.з 2
 
     public static void hello(String smb) {
         System.out.println("Hi " + smb + "!");
