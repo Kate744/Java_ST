66d608b52264edc2560107dfaf0bcba9fbaa6277
Kate744 <kate744@ya.ru>
2019-02-15 20:47:17 -0400

    Цикл для уравнения, ошибка else withoui if

--- addressbook-web-tests/src/test/java/stwtesting/autojava/Equation.java
+++ addressbook-web-tests/src/test/java/stwtesting/autojava/Equation.java
@@ -17,8 +17,58 @@ public class Equation {
 
         double d = b * b - 4 * a * c;
 
+   /*     if (a == 0) {
+            if (b == 0) {
+                if (c == 0) {
+                    n = -1;
+                } else {
+                    n = 0;
+                }
+            }
+            else {
+                    n = 1;
+                }
 
-        if (d > 0) {
+        } else {
+            if (d > 0) {
+                n = 2;
+
+            } else if (d == 0) {
+                n = 1;
+
+            } else {
+                n = 0;
+
+            }
+        }
+        }
+
+*/
+
+        if (a != 0) {
+            {
+                if (d > 0) {
+                    n = 2;
+
+                } else if (d == 0) {
+                    n = 1;
+
+                } else
+                {
+                    n = 0;
+                }
+
+            }    else if (b != 0) {
+                    n = 1;
+        } else if (c != 0) {
+                    n = 0;
+ }        else {
+                   n = -1;
+            }
+    }
+
+
+        /*if (d > 0) {
             n = 2;
 
         } else if (d == 0) {
@@ -28,7 +78,28 @@ public class Equation {
             n = 0;
 
         }
-    }
+    }*/
+
+       /* if (d > 0) {
+            n = 2;
+        } else if (d == 0) {
+            n = 1;
+        } else {
+            n = 0;
+        }
+    }*/
+
+       /* if (d > 0) {
+            n = 2;
+        }
+        if (d == 0) {
+            n = 1;
+        }
+        if (d < 0 ) {
+            n = 0;
+        }
+    }*/
+
 
     public int rootNumber () {
         return n;
