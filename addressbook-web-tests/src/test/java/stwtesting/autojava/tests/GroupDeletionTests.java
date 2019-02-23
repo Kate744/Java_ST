455e7288cadd7524eeea58e758784f3687909595
Kate744 <kate744@ya.ru>
2019-02-23 19:30:43 -0400

    Счетчик сравнения удаления, создания и модификации групп

--- addressbook-web-tests/src/test/java/stwtesting/autojava/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/stwtesting/autojava/tests/GroupDeletionTests.java
@@ -1,5 +1,6 @@
 package stwtesting.autojava.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.*;
 import stwtesting.autojava.model.GroupData;
 
@@ -9,13 +10,15 @@ public class GroupDeletionTests extends TestBase{
   public void testGroupDeletion() {
 
     app.getNavigationHelper().gotoGroupPage();
-
+    int before = app.getGroupHelper().getGroupCount();
     if (! app.getGroupHelper().isThereAGroup()) {
       app.getGroupHelper().createGroup(new GroupData("test2", null, null));
     }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroup();
     app.getGroupHelper().returntoGroupPage();
+    int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after, before - 1);
   }
 
 
--- addressbook-web-tests/src/test/java/stwtesting/autojava/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/stwtesting/autojava/tests/GroupModificationTests.java
@@ -1,5 +1,6 @@
 package stwtesting.autojava.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import stwtesting.autojava.model.GroupData;
 
@@ -7,8 +8,9 @@ public class GroupModificationTests extends TestBase {
 
     @Test
     public void testGroupModification() {
-        app.getNavigationHelper().gotoGroupPage();
 
+        app.getNavigationHelper().gotoGroupPage();
+        int before = app.getGroupHelper().getGroupCount();
         if (! app.getGroupHelper().isThereAGroup()) {
             app.getGroupHelper().createGroup(new GroupData("test2", null, null));
         }
@@ -18,6 +20,8 @@ public class GroupModificationTests extends TestBase {
         app.getGroupHelper().fillGroupForm(new GroupData("Test5", "Test8", "Test7"));
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returntoGroupPage();
+        int after = app.getGroupHelper().getGroupCount();
+        Assert.assertEquals(after, before);
 
     }
 }
