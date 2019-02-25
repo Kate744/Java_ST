a331097c18857fa68e1cf48a0edf0f25d3d9b07c
Kate744 <kate744@ya.ru>
2019-02-24 21:46:25 -0400

    Удаление контакта, сравнение полей контакта
    Удаление контакта, сравнение полей контакта:
    имя, фамилия, почта, юзернейм, группа, ДО удаления и ПОСЛЕ

--- addressbook-web-tests/src/test/java/stwtesting/autojava/model/NewContactData.java
+++ addressbook-web-tests/src/test/java/stwtesting/autojava/model/NewContactData.java
@@ -7,6 +7,37 @@ public class NewContactData {
     private final String email;
     private String group;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        NewContactData that = (NewContactData) o;
+
+        if (firstname != null ? !firstname.equals(that.firstname) : that.firstname != null) return false;
+        if (secondname != null ? !secondname.equals(that.secondname) : that.secondname != null) return false;
+        if (username != null ? !username.equals(that.username) : that.username != null) return false;
+        if (email != null ? !email.equals(that.email) : that.email != null) return false;
+        return group != null ? group.equals(that.group) : that.group == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = firstname != null ? firstname.hashCode() : 0;
+        result = 31 * result + (secondname != null ? secondname.hashCode() : 0);
+        result = 31 * result + (username != null ? username.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        result = 31 * result + (group != null ? group.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "NewContactData{" +
+                "firstname='" + firstname + '\'' +
+                '}';
+    }
+
     public NewContactData(String firstname, String secondname, String username, String email, String group) {
         this.firstname = firstname;
         this.secondname = secondname;
