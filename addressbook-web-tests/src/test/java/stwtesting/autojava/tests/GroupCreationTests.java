1d41f905d9647dc500497a07bac1afd77d314ed7
Kate744 <kate744@ya.ru>
2019-02-24 20:39:29 -0400

    GetGroup

--- addressbook-web-tests/src/test/java/stwtesting/autojava/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/stwtesting/autojava/tests/GroupCreationTests.java
@@ -4,6 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.*;
 import stwtesting.autojava.model.GroupData;
 
+import java.util.List;
+
 
 public class GroupCreationTests extends TestBase {
 
@@ -11,10 +13,14 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation() throws Exception {
 
     app.getNavigationHelper().gotoGroupPage();
-    int before = app.getGroupHelper().getGroupCount();
+
+    List<GroupData> before = app.getGroupHelper().getGroupList();
+
+    //int before = app.getGroupHelper().getGroupCount();
     app.getGroupHelper().createGroup(new GroupData("test5", null, null));
-    int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, before +1);
+    List<GroupData> after = app.getGroupHelper().getGroupList();
+    //int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after.size(), before.size() +1);
   }
 
 
