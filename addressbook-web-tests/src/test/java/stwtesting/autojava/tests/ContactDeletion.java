ccd880ee3782ad4550252bc78e56370dd0fed954
Kate744 <kate744@ya.ru>
2019-02-24 20:43:23 -0400

    Deletion

--- addressbook-web-tests/src/test/java/stwtesting/autojava/tests/ContactDeletion.java
+++ addressbook-web-tests/src/test/java/stwtesting/autojava/tests/ContactDeletion.java
@@ -9,12 +9,12 @@ public class ContactDeletion extends TestBase{
 
   @Test
   public void testContactDeletion() {
-    int before = app.getContactHelper().getContactCount();
+
     if (! app.getContactHelper().isThereAContact()) {
 
       app.getContactHelper().createNewContact(new NewContactData("Ivanko", "Ivankov", "jgii", "fjfi@m", "[none]"));
     }
-
+    int before = app.getContactHelper().getContactCount();
     app.getNavigationHelper().returnToHomePage();
     app.getContactHelper().selectUser(before-1);
     app.getContactHelper().submitDeletion();
