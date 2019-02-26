1c9e92b321a5e040ea1407271c9c63c42b04d43e
Kate744 <kate744@ya.ru>
2019-02-25 21:10:20 -0400

    1

--- addressbook-web-tests/src/test/java/stwtesting/autojava/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/stwtesting/autojava/tests/GroupModificationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import stwtesting.autojava.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificationTests extends TestBase {
@@ -19,11 +20,16 @@ public class GroupModificationTests extends TestBase {
         List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().selectGroup(before.size()-1);
         app.getGroupHelper().initGroupModification();
-        app.getGroupHelper().fillGroupForm(new GroupData("Test5", "Test8", "Test7"));
+        GroupData group = new GroupData("Test5", "Test8", "Test7");
+        app.getGroupHelper().fillGroupForm(group);
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returntoGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size());
+
+        before.remove(before.size()-1);
+        before.add(group);
+        Assert.assertEquals(new HashSet<>(before), new HashSet<>(after));
 
     }
 }
